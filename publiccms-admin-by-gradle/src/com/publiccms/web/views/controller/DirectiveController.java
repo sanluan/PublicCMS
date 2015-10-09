c5651e2a284bad0d87b5d8b9b457da61d5831d5c
kerneler <zyyy358@126.com>
2015-10-09 15:12:08 +0800

    指令接口bug修改

--- publiccms-admin-by-gradle/src/com/publiccms/web/views/controller/DirectiveController.java
+++ publiccms-admin-by-gradle/src/com/publiccms/web/views/controller/DirectiveController.java
@@ -1,9 +1,12 @@
 package com.publiccms.web.views.controller;
 
 import static com.publiccms.web.views.controller.IndexController.INTERFACE_NOT_FOUND;
+import static org.springframework.util.StringUtils.uncapitalize;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,22 +20,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sanluan.common.base.BaseController;
+import com.sanluan.common.handler.FreeMarkerExtendHandler;
 import com.sanluan.common.handler.HttpParameterHandler;
 import com.sanluan.common.servlet.HttpServletDirective;
 
 @Controller
 public class DirectiveController extends BaseController {
-	@Autowired
-	private Map<String, HttpServletDirective> directiveMap;
+	private Map<String, HttpServletDirective> actionMap = new HashMap<String, HttpServletDirective>();
 	@Autowired
 	private MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter;
+	@Autowired
+	private FreeMarkerExtendHandler freeMarkerExtendHandler;
 
 	private MediaType mediaType = new MediaType("application", "json", MappingJackson2HttpMessageConverter.DEFAULT_CHARSET);
 
 	@RequestMapping("directive.json")
 	public void directive(String action, String callback, HttpServletRequest request, HttpServletResponse response) {
 		try {
-			HttpServletDirective directive = directiveMap.get(action);
+			HttpServletDirective directive = actionMap.get(action);
 			if (notEmpty(directive)) {
 				directive.execute(mappingJackson2HttpMessageConverter, mediaType, request.getParameterMap(), callback, response);
 			} else {
@@ -48,8 +53,16 @@ public class DirectiveController extends BaseController {
 	@RequestMapping("directives.json")
 	@ResponseBody
 	public MappingJacksonValue directives(String callback) {
-		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(directiveMap.keySet());
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(actionMap.keySet());
 		mappingJacksonValue.setJsonpFunction(callback);
 		return mappingJacksonValue;
+	}
+
+	@Autowired
+	public void setActionMap(Map<String, HttpServletDirective> directiveMap) {
+		for (Entry<String, HttpServletDirective> entry : directiveMap.entrySet()) {
+			String directiveName = uncapitalize(entry.getKey().replaceAll(freeMarkerExtendHandler.getDirectiveRemoveRegex(), ""));
+			actionMap.put(directiveName, entry.getValue());
+		}
 	}
 }
