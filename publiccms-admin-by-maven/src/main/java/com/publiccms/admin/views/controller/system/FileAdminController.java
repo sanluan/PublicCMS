a37c7f4f9e2a0f4bf60163cbdf5567ccb56cdb2a
kerneler <zyyy358@126.com>
2015-10-21 12:03:33 +0800

    ie浏览器上传单个文件提示下载bug修复，其他优化

--- publiccms-admin-by-maven/src/main/java/com/publiccms/admin/views/controller/system/FileAdminController.java
+++ publiccms-admin-by-maven/src/main/java/com/publiccms/admin/views/controller/system/FileAdminController.java
@@ -1,17 +1,15 @@
 package com.publiccms.admin.views.controller.system;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.publiccms.common.tools.UserUtils;
@@ -29,21 +27,21 @@ public class FileAdminController extends BaseController {
     @Autowired
     private LogOperateService logOperateService;
 
-    @RequestMapping(value = { "upload" }, method = RequestMethod.POST)
-    @ResponseBody
-    public Map<String, Object> upload(MultipartFile file, String field, HttpServletRequest request, HttpSession session) {
-        Map<String, Object> map = new HashMap<String, Object>();
+    @RequestMapping(value = { "doUpload" }, method = RequestMethod.POST)
+    public String upload(MultipartFile file, String field, HttpServletRequest request, HttpSession session,
+            ModelMap model) {
         if (!file.isEmpty()) {
             String fileName = fileComponent.getUploadFileName(fileComponent.getSuffix(file.getOriginalFilename()));
             try {
                 fileComponent.upload(file, fileName);
-                map.put(field, fileName);
+                model.put("field", field);
+                model.put(field, fileName);
                 logOperateService.save(new LogOperate(UserUtils.getAdminFromSession(session).getId(),
                         LogOperateService.OPERATE_UPLOADFILE, RequestUtils.getIp(request), getDate(), fileName));
             } catch (IllegalStateException | IOException e) {
                 log.debug(e.getMessage());
             }
         }
-        return map;
+        return "common/upload";
     }
 }
