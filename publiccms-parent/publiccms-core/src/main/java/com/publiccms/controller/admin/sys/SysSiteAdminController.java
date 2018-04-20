adc7d252ce2504b6ee88192712d7b344a3bc8753
sanluan <zyyy358@126.com>
2018-04-20 11:16:53 +0800

    修复站点保存500异常bug

--- publiccms-parent/publiccms-core/src/main/java/com/publiccms/controller/admin/sys/SysSiteAdminController.java
+++ publiccms-parent/publiccms-core/src/main/java/com/publiccms/controller/admin/sys/SysSiteAdminController.java
@@ -108,7 +108,7 @@ public class SysSiteAdminController extends AbstractController {
                     || ControllerUtils.verifyCustom("unauthorizedDomain", !CmsVersion.verifyDomain(domainName), model)
                     || ControllerUtils.verifyNotEmpty("userName", userName, model)
                     || ControllerUtils.verifyNotEmpty("password", password, model)
-                    || ControllerUtils.verifyHasExist("domain", service.getEntity(domainName), model)) {
+                    || ControllerUtils.verifyHasExist("domain", domainService.getEntity(domainName), model)) {
                 return TEMPLATE_ERROR;
             }
             service.save(entity);
