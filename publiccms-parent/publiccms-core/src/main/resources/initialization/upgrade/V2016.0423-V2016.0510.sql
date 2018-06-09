8501a984afa3aa61f54882d811c2c69ef7294b5a
sanluan <zyyy358@126.com>
2018-06-09 10:14:51 +0800

    增加模块升级数据脚本

--- publiccms-parent/publiccms-core/src/main/resources/initialization/upgrade/V2016.0423-V2016.0510.sql
+++ publiccms-parent/publiccms-core/src/main/resources/initialization/upgrade/V2016.0423-V2016.0510.sql
@@ -1,7 +1,7 @@
 -- 20160414 --
-update `cms_category` set extend_id = NULL where extend_id = 0;
-update `cms_category_type` set extend_id = NULL where extend_id = 0;
-update `cms_model` set extend_id = NULL where extend_id = 0;
+UPDATE `cms_category` SET extend_id = NULL WHERE extend_id = 0;
+UPDATE `cms_category_type` SET extend_id = NULL WHERE extend_id = 0;
+UPDATE `cms_model` SET extend_id = NULL WHERE extend_id = 0;
 ALTER TABLE  `sys_user_token` ADD  `site_id` INT NOT NULL COMMENT  '站点ID' AFTER  `auth_token` , ADD INDEX (  `site_id` );
 ALTER TABLE  `sys_extend` ADD  `item_type` varchar(20) NOT NULL COMMENT '扩展类型' AFTER  `id`,ADD  `item_id` INT NOT NULL COMMENT  '扩展项目ID' AFTER  `item_type`;
 UPDATE  `sys_extend` SET  `item_type` =  'model',`item_id` =  7 WHERE  `sys_extend`.`id` =1;
@@ -17,22 +17,22 @@ ALTER TABLE `sys_dept_page` DROP `type`;
 RENAME TABLE  `cms_page_data` TO  `cms_place` ;
 RENAME TABLE  `cms_page_data_attribute` TO  `cms_place_attribute` ;
 ALTER TABLE  `cms_place_attribute` CHANGE  `page_data_id`  `place_id` INT( 11 ) NOT NULL COMMENT  '位置ID';
-update `sys_moudle` set `authorized_url`='cmsPlace/clear' where `id`=54;
-update `sys_moudle` set `authorized_url`='cmsPlace/check' where `id`=52;
-update `sys_moudle` set `authorized_url`='cmsPlace/refresh' where `id`=51;
-update `sys_moudle` set `authorized_url`='cmsPlace/delete' where `id`=50;
-update `sys_moudle` set `authorized_url`='cmsContent/lookup,cmsPage/lookup_content_list,file/doUpload,cmsPlace/save' where `id`=49;
-update `sys_moudle` set `authorized_url`='cmsPage/saveMetaData,file/doUpload,cmsPage/clearCache' where `id`=48;
+UPDATE `sys_moudle` SET `authorized_url`='cmsPlace/clear' WHERE `id`=54;
+UPDATE `sys_moudle` SET `authorized_url`='cmsPlace/check' WHERE `id`=52;
+UPDATE `sys_moudle` SET `authorized_url`='cmsPlace/refresh' WHERE `id`=51;
+UPDATE `sys_moudle` SET `authorized_url`='cmsPlace/delete' WHERE `id`=50;
+UPDATE `sys_moudle` SET `authorized_url`='cmsContent/lookup,cmsPage/lookup_content_list,file/doUpload,cmsPlace/save' where `id`=49;
+UPDATE `sys_moudle` SET `authorized_url`='cmsPage/saveMetaData,file/doUpload,cmsPage/clearCache' WHERE `id`=48;
 -- 20160504 --
 ALTER TABLE  `cms_content_attribute` CHANGE  `text`  `text` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT  '内容';
 -- 20160506 --
 ALTER TABLE  `cms_category` CHANGE  `english_name`  `code` VARCHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT  '编码';
 ALTER TABLE  `cms_content_attribute` CHANGE  `text`  `text` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT  '内容';
-update `cms_category` set path = replace(path,'englishName','code'),content_path=replace(content_path,'englishName','code');
+UPDATE `cms_category` SET path = replace(path,'englishName','code'),content_path=replace(content_path,'englishName','code');
 -- 20160509 --
 ALTER TABLE  `cms_category` CHANGE  `content_path`  `content_path` VARCHAR( 500 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT  '内容路径';
 ALTER TABLE  `cms_category` CHANGE  `template_path`  `template_path` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT  '模板路径';
 INSERT INTO `sys_moudle` VALUES ('30', '<i class=\"icon-globe icon-large\"></i> 页面管理', 'cmsPage/placeList', 'sysUser/lookup,sysUser/lookup_content_list,cmsPage/placeDataList,cmsPage/placeDataAdd,cmsPlace/save,cmsTemplate/publishPlace,cmsPage/publishPlace,cmsPage/push_page,cmsPage/push_page_list', '4', '0');
-update  `sys_moudle` set `authorized_url` ='cmsContent/push_content,cmsContent/push_content_list,cmsContent/push_to_content,cmsContent/push_page,cmsContent/push_page_list,cmsPage/placeDataAdd,cmsPlace/save,cmsContent/related' WHERE id = 23;
-delete from `sys_moudle`  where id = 29;
+UPDATE  `sys_moudle` SET `authorized_url` ='cmsContent/push_content,cmsContent/push_content_list,cmsContent/push_to_content,cmsContent/push_page,cmsContent/push_page_list,cmsPage/placeDataAdd,cmsPlace/save,cmsContent/related' WHERE id = 23;
+DELETE FROM `sys_moudle`  WHERE id = 29;
 INSERT INTO `sys_moudle` VALUES ('29', '推荐', 'cmsCategory/push_page', 'cmsCategory/push_page_list,cmsPage/placeDataAdd,cmsPlace/save', '24', '0');
