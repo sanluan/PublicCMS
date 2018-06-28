f5e6eaeb4d46014df599be99328fa910dc44453d
sanluan <zyyy358@126.com>
2018-06-28 10:20:35 +0800

    https://github.com/sanluan/PublicCMS/issues/13 Unsafe Unzip bug fix

--- publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/tools/ZipUtils.java
+++ publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/tools/ZipUtils.java
@@ -144,11 +144,15 @@ public class ZipUtils implements Base {
         if (null != entryEnum) {
             while (entryEnum.hasMoreElements()) {
                 ZipEntry zipEntry = entryEnum.nextElement();
+                String filePath = zipEntry.getName();
+                if (filePath.contains("..")) {
+                    filePath = filePath.replace("..", BLANK);
+                }
                 if (zipEntry.isDirectory()) {
-                    File dir = new File(targetPath + File.separator + zipEntry.getName());
+                    File dir = new File(targetPath + File.separator + filePath);
                     dir.mkdirs();
                 } else {
-                    File targetFile = new File(targetPath + File.separator + zipEntry.getName());
+                    File targetFile = new File(targetPath + File.separator + filePath);
                     if (!targetFile.exists() || overwrite) {
                         targetFile.getParentFile().mkdirs();
                         try (InputStream inputStream = zipFile.getInputStream(zipEntry);
