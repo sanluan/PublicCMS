21007980b3664706d02667220c82505edfff5b13
sanluan <master@sanluan.com>
2018-07-26 10:04:35 +0800

    删除默认日期转换

--- publiccms-parent/publiccms-core/src/main/java/com/publiccms/common/base/AbstractController.java
+++ publiccms-parent/publiccms-core/src/main/java/com/publiccms/common/base/AbstractController.java
@@ -1,17 +1,11 @@
 package com.publiccms.common.base;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 
 import com.publiccms.entities.sys.SysDomain;
 import com.publiccms.entities.sys.SysSite;
@@ -39,12 +33,5 @@ public abstract class AbstractController {
 
 	protected SysSite getSite(HttpServletRequest request) {
 		return siteComponent.getSite(request.getServerName());
-	}
-
-	@InitBinder
-	public void initBinder(WebDataBinder binder) {
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true)); // true:允许输入空值，false:不能为空值
-
 	}
 }
