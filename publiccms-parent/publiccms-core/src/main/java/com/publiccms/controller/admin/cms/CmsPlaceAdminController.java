3f13edac24f817472c87351efefc7cc7ec3668d3
sanluan <master@sanluan.com>
2018-09-29 10:09:59 +0800

    页面片段数据取消审核bug修复

--- publiccms-parent/publiccms-core/src/main/java/com/publiccms/controller/admin/cms/CmsPlaceAdminController.java
+++ publiccms-parent/publiccms-core/src/main/java/com/publiccms/controller/admin/cms/CmsPlaceAdminController.java
@@ -214,7 +214,7 @@ public class CmsPlaceAdminController extends AbstractController {
         }
         if (CommonUtils.notEmpty(ids)) {
             SysSite site = getSite(request);
-            service.check(site.getId(), ids, path);
+            service.uncheck(site.getId(), ids, path);
             logOperateService.save(new LogOperate(site.getId(), ControllerUtils.getAdminFromSession(session).getId(),
                     LogLoginService.CHANNEL_WEB_MANAGER, "check.place", RequestUtils.getIpAddress(request), CommonUtils.getDate(),
                     StringUtils.join(ids, ',')));
