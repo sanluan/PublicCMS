aa56028655e4857e635393363b921a065dba354b
sanluan <master@sanluan.com>
2018-10-01 19:14:16 +0800

    增加遍历本地IP方法

--- publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/tools/IpUtils.java
+++ publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/tools/IpUtils.java
@@ -1,5 +1,11 @@
 package com.publiccms.common.tools;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class IpUtils {
@@ -8,15 +14,50 @@ public class IpUtils {
     private static Pattern IPV4_PATTERN = Pattern
             .compile("^(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}$");
 
+    /**
+     * @param value
+     * @return
+     */
     public static boolean isIpv4(String value) {
         return IPV4_PATTERN.matcher(value).matches();
     }
 
+    /**
+     * @param value
+     * @return
+     */
     public static boolean isIpv6(String value) {
         return IPV6_PATTERN.matcher(value).matches();
     }
 
+    /**
+     * @param value
+     * @return
+     */
     public static boolean isIp(String value) {
         return isIpv4(value) || isIpv6(value);
+    }
+
+    /**
+     * 
+     * 
+     * @return 本机IP地址
+     * @throws SocketException
+     */
+    public static List<String> getLocalIpAddressList() throws SocketException {
+        List<String> rsList = new ArrayList<>();
+        Enumeration<NetworkInterface> interfaces = null;
+        interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface ni = interfaces.nextElement();
+            Enumeration<InetAddress> addresss = ni.getInetAddresses();
+            while (addresss.hasMoreElements()) {
+                InetAddress nextElement = addresss.nextElement();
+                if (!nextElement.isLoopbackAddress() && !nextElement.isMulticastAddress() && !nextElement.isLinkLocalAddress()) {
+                    rsList.add(nextElement.getHostAddress());
+                }
+            }
+        }
+        return rsList;
     }
 }
