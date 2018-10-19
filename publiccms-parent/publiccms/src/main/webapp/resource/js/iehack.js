c6132a52543da31f6c20549fd6b0ed1858bfa49a
sanluan <master@sanluan.com>
2018-10-19 11:23:51 +0800

    ie不支持数组indexOf方法兼容性改进

---
+++ publiccms-parent/publiccms/src/main/webapp/resource/js/iehack.js
@@ -0,0 +1,18 @@
+if (!Array.prototype.indexOf){
+  Array.prototype.indexOf = function(elt /*, from*/){
+    var len = this.length >>> 0;
+
+    var from = Number(arguments[1]) || 0;
+    from = (from < 0)
+         ? Math.ceil(from)
+         : Math.floor(from);
+    if (from < 0)
+      from += len;
+
+    for (; from < len; from++){
+      if (from in this && this[from] === elt)
+        return from;
+    }
+    return -1;
+  };
+}
