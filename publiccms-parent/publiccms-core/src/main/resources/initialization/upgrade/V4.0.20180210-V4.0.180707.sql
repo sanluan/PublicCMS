287a5c51263820ead60ea0cb668abddd4be397f0
sanluan <master@sanluan.com>
2018-12-06 17:27:29 +0800

    sql注释文字修改

--- publiccms-parent/publiccms-core/src/main/resources/initialization/upgrade/V4.0.20180210-V4.0.180707.sql
+++ publiccms-parent/publiccms-core/src/main/resources/initialization/upgrade/V4.0.20180210-V4.0.180707.sql
@@ -1,6 +1,6 @@
 UPDATE `sys_module` SET `authorized_url` = 'sysUser/lookup,cmsContent/recycle,cmsContent/realDelete' WHERE  `id` = 117;
 ALTER TABLE `sys_user` MODIFY COLUMN `last_login_ip` varchar(64) NULL DEFAULT NULL COMMENT '最后登录ip' AFTER `last_login_date`;
-ALTER TABLE `sys_user_token` MODIFY COLUMN `login_ip` varchar(64) NOT NULL COMMENT '登陆IP' AFTER `create_date`;
+ALTER TABLE `sys_user_token` MODIFY COLUMN `login_ip` varchar(64) NOT NULL COMMENT '登录IP' AFTER `create_date`;
 -- 20180414 --
 DROP TABLE IF EXISTS `home_active`;
 DROP TABLE IF EXISTS `home_article`;
