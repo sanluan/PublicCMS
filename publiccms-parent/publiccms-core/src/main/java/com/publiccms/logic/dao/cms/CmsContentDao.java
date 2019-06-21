8195a6cd8e1e2f04cd298917326ced24d87a3d14
sanluan <master@publiccms.com>
2019-06-21 14:57:33 +0800

    删除冗余代码

--- publiccms-parent/publiccms-core/src/main/java/com/publiccms/logic/dao/cms/CmsContentDao.java
+++ publiccms-parent/publiccms-core/src/main/java/com/publiccms/logic/dao/cms/CmsContentDao.java
@@ -227,19 +227,6 @@ public class CmsContentDao extends BaseDao<CmsContent> {
         if (null != queryEntitry.getDisabled()) {
             queryHandler.condition("bean.disabled = :disabled").setParameter("disabled", queryEntitry.getDisabled());
         }
-        if (CommonUtils.notEmpty(queryEntitry.getParentId())) {
-            queryHandler.condition("bean.parentId = :parentId").setParameter("parentId", queryEntitry.getParentId());
-        } else {
-            if (CommonUtils.notEmpty(queryEntitry.getCategoryIds())) {
-                queryHandler.condition("bean.categoryId in (:categoryIds)").setParameter("categoryIds",
-                        queryEntitry.getCategoryIds());
-            } else if (CommonUtils.notEmpty(queryEntitry.getCategoryId())) {
-                queryHandler.condition("bean.categoryId = :categoryId").setParameter("categoryId", queryEntitry.getCategoryId());
-            }
-            if (null != queryEntitry.getEmptyParent() && queryEntitry.getEmptyParent()) {
-                queryHandler.condition("bean.parentId is null");
-            }
-        }
         if (CommonUtils.notEmpty(queryEntitry.getQuoteId())) {
             queryHandler.condition("bean.quoteContentId = :quoteContentId").setParameter("quoteContentId",
                     queryEntitry.getQuoteId());
