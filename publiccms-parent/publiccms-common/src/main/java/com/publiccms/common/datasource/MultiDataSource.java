71c2849a9619902a9ea17d9ebd436c2560cb5ba3
sanluan <master@sanluan.com>
2019-11-26 11:41:12 +0800

    增加动态方言类

--- publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/datasource/MultiDataSource.java
+++ publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/datasource/MultiDataSource.java
@@ -3,13 +3,17 @@ package com.publiccms.common.datasource;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 /**
- * <p>多数据源解决方案</p>
+ * <p>
+ * 多数据源解决方案
+ * </p>
  * 
- * <p>MultiDataSource</p> 
+ * <p>
+ * MultiDataSource
+ * </p>
  *
  */
 public class MultiDataSource extends AbstractRoutingDataSource {
-    
+
     private static final ThreadLocal<String> HOLDER = new ThreadLocal<>();
 
     @Override
@@ -18,7 +22,26 @@ public class MultiDataSource extends AbstractRoutingDataSource {
     }
 
     /**
-     * @param name <p>数据源名称</p><p>datasource name</p>
+     * @return name
+     *         <p>
+     *         数据源名称
+     *         </p>
+     *         <p>
+     *         datasource name
+     *         </p>
+     */
+    public static String getDataSourceName() {
+        return HOLDER.get();
+    }
+
+    /**
+     * @param name
+     *            <p>
+     *            数据源名称
+     *            </p>
+     *            <p>
+     *            datasource name
+     *            </p>
      */
     public static void setDataSourceName(String name) {
         HOLDER.set(name);
