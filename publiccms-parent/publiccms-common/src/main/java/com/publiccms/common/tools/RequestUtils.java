218fa3fccd89c9da4204f0f4e3cf6b4e66e3a513
sanluan <master@publiccms.com>
2020-02-17 17:47:10 +0800

    增加https相关配置

--- publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/tools/RequestUtils.java
+++ publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/tools/RequestUtils.java
@@ -135,26 +135,7 @@ public class RequestUtils {
      */
     public static String getIpAddress(HttpServletRequest request) {
         if (null != request) {
-            String localIp = request.getRemoteAddr();
-            String ip = request.getHeader("X-Real-IP");
-            if (CommonUtils.notEmpty(ip) && !"unknown".equalsIgnoreCase(ip)) {
-                if (ip.length() > 64) {
-                    ip = ip.substring(0, 64);
-                }
-                return ip.equals(localIp) ? ip : ip + "," + localIp;
-            }
-            ip = request.getHeader("X-Forwarded-For");
-            if (CommonUtils.notEmpty(ip) && !"unknown".equalsIgnoreCase(ip)) {
-                int index = ip.indexOf(Constants.COMMA);
-                if (index != -1) {
-                    return ip.substring(0, index);
-                }
-                if (ip.length() > 64) {
-                    ip = ip.substring(0, 64);
-                }
-                return ip.equals(localIp) ? ip : ip + "," + localIp;
-            }
-            return localIp;
+            return request.getRemoteAddr();
         }
         return null;
     }
