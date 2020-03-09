f07e3ae5ff4304462c173e3c961c586358b4dd68
sanluan <master@publiccms.com>
2020-03-09 17:01:19 +0800

    升级ckeditor到4.14

--- publiccms-parent/publiccms/src/main/webapp/resource/ckeditor/config.js
+++ publiccms-parent/publiccms/src/main/webapp/resource/ckeditor/config.js
@@ -1,11 +1,12 @@
 /**
- * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
     config.language = window.CKEDITOR_LANGUAGE;
+  // config.language = 'fr';
 	// config.uiColor = '#AADC6E';
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
     config.extraPlugins = 'codesnippet';
