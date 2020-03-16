f0ec8f2d4293574927defe6a5e6ac494ae0a8ba1
sanluan <master@publiccms.com>
2020-03-16 12:28:34 +0800

    文件路径兼容相对路径

--- publiccms-parent/publiccms-core/src/main/java/com/publiccms/logic/component/template/TemplateComponent.java
+++ publiccms-parent/publiccms-core/src/main/java/com/publiccms/logic/component/template/TemplateComponent.java
@@ -200,7 +200,7 @@ public class TemplateComponent implements Cache {
             entity.setUrl(getUrl(site, entity.isHasStatic(), entity.getUrl()));
         }
     }
-    
+
     /**
      * @param site
      * @param entity
@@ -216,7 +216,7 @@ public class TemplateComponent implements Cache {
      * @return
      */
     public String getUrl(SysSite site, boolean hasStatic, String url) {
-        if (CommonUtils.empty(url) || url.contains("://") || url.startsWith("//")) {
+        if (CommonUtils.empty(url) || url.contains("://") || url.startsWith("/")) {
             return url;
         } else {
             return hasStatic ? site.getSitePath() + url : site.getDynamicPath() + url;
