fedf94af2f1bb79c6c89b81540e9d7d916fb11da
sanluan <master@publiccms.com>
2020-04-08 09:35:14 +0800

    第三方登录模板示例与功能修复

--- publiccms-parent/publiccms-oauth/src/main/java/com/publiccms/view/directive/oauth/OauthListDirective.java
+++ publiccms-parent/publiccms-oauth/src/main/java/com/publiccms/view/directive/oauth/OauthListDirective.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.publiccms.common.api.oauth.OauthGateway;
-import com.publiccms.common.base.AbstractTaskDirective;
+import com.publiccms.common.base.AbstractTemplateDirective;
 import com.publiccms.common.handler.RenderHandler;
 import com.publiccms.entities.sys.SysSite;
 
@@ -18,7 +18,7 @@ import com.publiccms.entities.sys.SysSite;
  * 
  */
 @Component
-public class OauthListDirective extends AbstractTaskDirective {
+public class OauthListDirective extends AbstractTemplateDirective {
     @Override
     public void execute(RenderHandler handler) throws IOException, Exception {
         SysSite site = getSite(handler);
