4f801e45fcb520c7ea06464d108480efafa8c989
sanluan <master@publiccms.com>
2020-06-18 20:20:50 +0800

    分类静态化最大页码保护

--- publiccms-parent/publiccms-core/src/main/java/com/publiccms/logic/service/cms/CmsContentService.java
+++ publiccms-parent/publiccms-core/src/main/java/com/publiccms/logic/service/cms/CmsContentService.java
@@ -422,6 +422,9 @@ public class CmsContentService extends BaseService<CmsContent> {
                     quote.setCheckUserId(entity.getCheckUserId());
                     quote.setCheckDate(entity.getCheckDate());
                     quote.setPublishDate(entity.getPublishDate());
+                    quote.setHasStatic(entity.isHasStatic());
+                    quote.setHasFiles(entity.isHasFiles());
+                    quote.setHasImages(entity.isHasImages());
                 } else {
                     delete(quote.getId());
                 }
