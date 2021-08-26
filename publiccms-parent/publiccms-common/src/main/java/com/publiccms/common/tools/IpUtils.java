3e2d14678fcb63b05ed54a13b8f29d471d3b6876
sanluan <master@publiccms.com>
2021-08-26 17:49:39 +0800

    增加ipv4转数字方法

--- publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/tools/IpUtils.java
+++ publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/tools/IpUtils.java
@@ -2,11 +2,33 @@ package com.publiccms.common.tools;
 
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class IpUtils {
     private static Pattern IPV6_PATTERN = Pattern.compile(
             "^\\[?((([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){1,7}:)|(([0-9A-Fa-f]{1,4}:){6}:[0-9A-Fa-f]{1,4})|(([0-9A-Fa-f]{1,4}:){5}(:[0-9A-Fa-f]{1,4}){1,2})|(([0-9A-Fa-f]{1,4}:){4}(:[0-9A-Fa-f]{1,4}){1,3})|(([0-9A-Fa-f]{1,4}:){3}(:[0-9A-Fa-f]{1,4}){1,4})|(([0-9A-Fa-f]{1,4}:){2}(:[0-9A-Fa-f]{1,4}){1,5})|([0-9A-Fa-f]{1,4}:(:[0-9A-Fa-f]{1,4}){1,6})|(:(:[0-9A-Fa-f]{1,4}){1,7})|(([0-9A-Fa-f]{1,4}:){6}(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3})|(([0-9A-Fa-f]{1,4}:){5}:(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3})|(([0-9A-Fa-f]{1,4}:){4}(:[0-9A-Fa-f]{1,4}){0,1}:(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3})|(([0-9A-Fa-f]{1,4}:){3}(:[0-9A-Fa-f]{1,4}){0,2}:(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3})|(([0-9A-Fa-f]{1,4}:){2}(:[0-9A-Fa-f]{1,4}){0,3}:(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3})|([0-9A-Fa-f]{1,4}:(:[0-9A-Fa-f]{1,4}){0,4}:(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3})|(:(:[0-9A-Fa-f]{1,4}){0,5}:(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}))\\]?$");
     private static Pattern IPV4_PATTERN = Pattern
             .compile("^(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}$");
+
+    /**
+     * @param value
+     * @return number
+     */
+    public static long getIpv4Number(String value) {
+        String[] array = StringUtils.split(value, '.');
+        long result = 0;
+        if (null != array && 4 == array.length) {
+            try {
+                result = Integer.parseInt(array[0]) * 256 * 256 * 256;
+                result += Integer.parseInt(array[1]) * 256 * 256;
+                result += Integer.parseInt(array[2]) * 256;
+                result += Integer.parseInt(array[3]);
+            } catch (NumberFormatException e) {
+                result = 0;
+            }
+        }
+        return result;
+    }
 
     /**
      * @param value
@@ -31,5 +53,5 @@ public class IpUtils {
     public static boolean isIp(String value) {
         return isIpv4(value) || isIpv6(value);
     }
-    
+
 }
---
+++ publiccms-parent/publiccms-core/src/main/java/com/publiccms/views/method/tools/GetIpv4NumberMethod.java
@@ -0,0 +1,40 @@
+package com.publiccms.views.method.tools;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.publiccms.common.base.BaseMethod;
+import com.publiccms.common.tools.CommonUtils;
+import com.publiccms.common.tools.IpUtils;
+
+import freemarker.template.TemplateModelException;
+
+/**
+ *
+ * GetMd5Method
+ * 
+ */
+@Component
+public class GetIpv4NumberMethod extends BaseMethod {
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Object exec(@SuppressWarnings("rawtypes") List arguments) throws TemplateModelException {
+        String string = getString(0, arguments);
+        if (CommonUtils.notEmpty(string)) {
+            return IpUtils.getIpv4Number(string);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean needAppToken() {
+        return false;
+    }
+
+    @Override
+    public int minParametersNumber() {
+        return 1;
+    }
+}
