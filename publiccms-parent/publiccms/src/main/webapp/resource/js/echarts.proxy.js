a38c4a96a97af89a194d35220bf5fbbbe6e968f8
sanluan <master@publiccms.com>
2022-04-08 13:56:37 +0800

    echarts代理优化

--- publiccms-parent/publiccms/src/main/webapp/resource/js/echarts.proxy.js
+++ publiccms-parent/publiccms/src/main/webapp/resource/js/echarts.proxy.js
@@ -1,22 +1,26 @@
 var echartsProxyList=[];
-var echarts={
-        index:-1,
-        list:[],
-        init:function(ele){
-            this.list.push({ele:ele});
-            this.index++;
-            return this;
-        },
-        setOption:function(config){
-            this.list[this.index].config=config;
-            var chart=this.list[this.index];
-            echartsProxyList.push(function(echarts){
-                echarts.init(chart.ele).setOption(chart.config);
+function ChartProxy(ele) {
+    this.ele = ele;
+    this.setOption = function(config) {
+        this.config=config;
+        var chart=this;
+        if( chart.echart) {
+            return chart.echart.setOption(chart.config);
+        } else {
+            echartsProxyList.push(function(echarts) {
+                chart.echart=echarts.init(chart.ele).setOption(chart.config);
             });
+            return chart;
         }
+    }
 }
-function echartsProxy(echarts){
-    if(echartsProxyList){
+var echarts={
+    init:function(ele) {
+        return new ChartProxy(ele);
+    }
+}
+function echartsProxy(echarts) {
+    if(echartsProxyList) {
         for(var i=0;i<echartsProxyList.length;i++){
             echartsProxyList[i](echarts);
         }
