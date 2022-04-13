efb230ed99f0364aebcb5c0f2160fbd72fb41f33
sanluan <master@publiccms.com>
2022-04-13 10:30:40 +0800

    百度编辑器升级

--- publiccms-parent/publiccms/src/main/webapp/resource/ueditor/ueditor.parse.js
+++ publiccms-parent/publiccms/src/main/webapp/resource/ueditor/ueditor.parse.js
@@ -1,7 +1,7 @@
 /*!
  * UEditor
  * version: ueditor
- * build: Fri Dec 17 2021 09:59:54 GMT+0800 (中国标准时间)
+ * build: Wed Apr 13 2022 10:26:23 GMT+0800 (中国标准时间)
  */
 
 (function(){
@@ -889,6 +889,34 @@ UE.parse.register('background', function (utils) {
 
     //追加默认的表格样式
     styles && utils.cssRule('ueditor_background', me.selector + '{' + styles + '}', document);
+});
+UE.parse.register('vedio',function(utils){
+    var video = this.root.getElementsByTagName('video'),
+        audio = this.root.getElementsByTagName('audio');
+
+    document.createElement('video');document.createElement('audio');
+    if(video.length || audio.length){
+        var sourcePath = utils.removeLastbs(this.rootPath),
+            jsurl = sourcePath + '/third-party/video-js/video.min.js',
+            cssurl = sourcePath + '/third-party/video-js/video-js.css'
+
+        if(!window.videojs) {
+            utils.loadFile(document,{
+                id : "video_css",
+                tag : "link",
+                rel : "stylesheet",
+                type : "text/css",
+                href : cssurl
+            });
+            utils.loadFile(document,{
+                id : "video_js",
+                src : jsurl,
+                tag : "script",
+                type : "text/javascript"
+            });
+        }
+
+    }
 });
 UE.parse.register('list',function(utils){
     var customCss = [],
