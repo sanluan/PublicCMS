fbb9e488383880867795e65e79e4f94ee04710c4
sanluan <master@publiccms.com>
2022-04-17 16:48:49 +0800

    修改统计js

--- publiccms-parent/publiccms/src/main/webapp/resource/js/cms.analytics.js
+++ publiccms-parent/publiccms/src/main/webapp/resource/js/cms.analytics.js
@@ -9,7 +9,7 @@ if (typeof window.cmsAnalytics !== 'object') {
     }
     window.cmsAnalytics = (function(){
         var cmsAnalytics={};
-        cmsAnalytics.sessionIdName='cmsAnalyticsSessionId';
+        cmsAnalytics.sessionIdName='PUBLICCMS_ANALYTICS_ID';
         function guid() {
             return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
                 var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
