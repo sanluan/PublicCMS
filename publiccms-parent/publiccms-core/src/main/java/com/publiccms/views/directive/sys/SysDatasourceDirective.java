d8d7626cf51e4968fb384e1637a3c0c9921f33e9
sanluan <master@publiccms.com>
2022-04-22 19:14:05 +0800

    安全漏洞修复 感谢 姜洋-长亭科技

--- publiccms-parent/publiccms-core/src/main/java/com/publiccms/views/directive/sys/SysDatasourceDirective.java
+++ publiccms-parent/publiccms-core/src/main/java/com/publiccms/views/directive/sys/SysDatasourceDirective.java
@@ -45,6 +45,11 @@ public class SysDatasourceDirective extends AbstractTemplateDirective {
         }
     }
 
+    @Override
+    public boolean needAppToken() {
+        return true;
+    }
+
     @Autowired
     private SysDatasourceService service;
 
