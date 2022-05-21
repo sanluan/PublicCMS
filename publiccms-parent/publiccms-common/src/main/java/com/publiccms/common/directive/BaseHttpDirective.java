a8ddae93fd22731180cfe782386aef7a371b2458
sanluan <master@publiccms.com>
2022-05-21 23:38:38 +0800

    框架版本更新,增加指令命名空间

--- publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/directive/BaseHttpDirective.java
+++ publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/directive/BaseHttpDirective.java
@@ -13,18 +13,23 @@ import com.publiccms.common.handler.HttpParameterHandler;
 /**
  * 指令基类
  * 
- * Base directive class 
+ * Base directive class
  *
  */
 public abstract class BaseHttpDirective implements HttpDirective, Directive {
     private String name;
-    
+    private String shortName;
+    private String namespace;
+
     @Override
     public void execute(HttpMessageConverter<Object> httpMessageConverter, MediaType mediaType, HttpServletRequest request,
             HttpServletResponse response) throws IOException, Exception {
         execute(new HttpParameterHandler(httpMessageConverter, mediaType, request, response));
     }
 
+    /**
+     * @return the name
+     */
     public String getName() {
         return this.name;
     }
@@ -34,5 +39,31 @@ public abstract class BaseHttpDirective implements HttpDirective, Directive {
      */
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String getNamespace() {
+        return namespace;
+    }
+
+    /**
+     * @param namespace
+     *            the namespace to set
+     */
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+    }
+
+    @Override
+    public String getShortName() {
+        return shortName;
+    }
+
+    /**
+     * @param shortName
+     *            the shortName to set
+     */
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
     }
 }
--- publiccms-parent/publiccms-core/src/main/java/com/publiccms/logic/component/site/DirectiveComponent.java
+++ publiccms-parent/publiccms-core/src/main/java/com/publiccms/logic/component/site/DirectiveComponent.java
@@ -17,6 +17,7 @@ import com.publiccms.common.base.AbstractTaskDirective;
 import com.publiccms.common.base.AbstractTemplateDirective;
 import com.publiccms.common.base.BaseMethod;
 import com.publiccms.common.constants.CommonConstants;
+import com.publiccms.common.directive.BaseTemplateDirective;
 import com.publiccms.logic.component.template.NoCacheDirective;
 import com.publiccms.logic.component.template.TemplateCacheComponent;
 import com.publiccms.logic.component.template.TemplateComponent;
@@ -31,11 +32,12 @@ import freemarker.template.TemplateModelException;
  *
  */
 public class DirectiveComponent {
+    public static final String DEFAULT_NAMESPACE = "cms";
     private String directiveRemoveRegex;
     private String methodRemoveRegex;
     private String directivePrefix;
     protected final Log log = LogFactory.getLog(getClass());
-
+    private Map<String, Map<String, BaseTemplateDirective>> namespaceMap = new HashMap<>();
     private Map<String, AbstractTemplateDirective> templateDirectiveMap = new HashMap<>();
     private Map<String, AbstractTaskDirective> taskDirectiveMap = new HashMap<>();
     private Map<String, BaseMethod> methodMap = new HashMap<>();
@@ -49,6 +51,26 @@ public class DirectiveComponent {
         return StringUtils.uncapitalize(className.replaceAll(directiveRemoveRegex, CommonConstants.BLANK));
     }
 
+    public static void main(String[] args) {
+
+    }
+
+    private String getDirectiveNamespace(Class<? extends BaseTemplateDirective> clazz) {
+        String packagename = clazz.getPackage().getName();
+        if (packagename.contains(CommonConstants.DOT)) {
+            return packagename.substring(packagename.lastIndexOf(CommonConstants.DOT) + 1);
+        }
+        return DEFAULT_NAMESPACE;
+    }
+
+    private String getDirectiveShortName(String namespace, String className) {
+        if (className.toLowerCase().startsWith(namespace)) {
+            return StringUtils.uncapitalize(className.substring(namespace.length(), className.length())
+                    .replaceAll(directiveRemoveRegex, CommonConstants.BLANK));
+        }
+        return StringUtils.uncapitalize(className.replaceAll(directiveRemoveRegex, CommonConstants.BLANK));
+    }
+
     @Autowired
     private void init(FreeMarkerConfigurer freeMarkerConfigurer, List<AbstractTemplateDirective> templateDirectiveList,
             List<AbstractTaskDirective> taskDirectiveList, List<BaseMethod> methodList)
@@ -57,6 +79,15 @@ public class DirectiveComponent {
             if (null == directive.getName()) {
                 directive.setName(getDirectiveName(directive.getClass().getSimpleName()));
             }
+            if (null == directive.getNamespace()) {
+                directive.setNamespace(getDirectiveNamespace(directive.getClass()));
+            }
+            if (null == directive.getShortName()) {
+                directive.setShortName(getDirectiveShortName(directive.getNamespace(), directive.getClass().getSimpleName()));
+            }
+            Map<String, BaseTemplateDirective> directiveMap = namespaceMap.computeIfAbsent(directive.getNamespace(),
+                    k -> new HashMap<>());
+            directiveMap.put(directive.getShortName(), directive);
             templateDirectiveMap.put(directive.getName(), directive);
         }
         log.info(new StringBuilder().append(templateDirectiveMap.size()).append(" template directives created:")
@@ -65,10 +96,25 @@ public class DirectiveComponent {
             if (null == directive.getName()) {
                 directive.setName(getDirectiveName(directive.getClass().getSimpleName()));
             }
+            if (null == directive.getNamespace()) {
+                directive.setNamespace(getDirectiveNamespace(directive.getClass()));
+            }
+            if (null == directive.getShortName()) {
+                directive.setShortName(getDirectiveShortName(directive.getNamespace(), directive.getClass().getSimpleName()));
+            }
+            Map<String, BaseTemplateDirective> directiveMap = namespaceMap.computeIfAbsent(directive.getNamespace(),
+                    k -> new HashMap<>());
+            directiveMap.put(directive.getShortName(), directive);
             taskDirectiveMap.put(directive.getName(), directive);
         }
-        log.info(new StringBuilder().append(taskDirectiveMap.size()).append(" task directives created:")
+        log.info(new StringBuilder().append(taskDirectiveMap.size()).append(" task directives created : ")
                 .append(taskDirectiveMap.keySet()).toString());
+        log.info(new StringBuilder().append(namespaceMap.size()).append(" namespace created :").append(namespaceMap.keySet())
+                .toString());
+        for (Entry<String, Map<String, BaseTemplateDirective>> entry : namespaceMap.entrySet()) {
+            log.info(new StringBuilder().append("namespace ").append(entry.getKey()).append(" has ").append(entry.getValue().keySet().size())
+                    .append(" directives : ").append(entry.getValue().keySet()).toString());
+        }
         for (BaseMethod method : methodList) {
             if (null == method.getName()) {
                 method.setName(StringUtils
@@ -87,10 +133,13 @@ public class DirectiveComponent {
         for (Entry<String, AbstractTemplateDirective> entry : getTemplateDirectiveMap().entrySet()) {
             freemarkerVariables.put(directivePrefix + entry.getKey(), entry.getValue());
         }
+        for (Entry<String, Map<String, BaseTemplateDirective>> entry : getNamespaceMap().entrySet()) {
+            freemarkerVariables.put(entry.getKey(), entry.getValue());
+        }
         freemarkerVariables.putAll(methodMap);
         adminConfiguration.setAllSharedVariables(new SimpleHash(freemarkerVariables, adminConfiguration.getObjectWrapper()));
         templateComponent.setAdminConfiguration(adminConfiguration);
-        
+
         Configuration webConfiguration = new Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
         File webFile = new File(siteComponent.getWebTemplateFilePath());
         webFile.mkdirs();
@@ -147,6 +196,13 @@ public class DirectiveComponent {
 
     public void setDirectivePrefix(String directivePrefix) {
         this.directivePrefix = directivePrefix;
+    }
+
+    /**
+     * @return the namespaceMap
+     */
+    public Map<String, Map<String, BaseTemplateDirective>> getNamespaceMap() {
+        return namespaceMap;
     }
 
     /**
