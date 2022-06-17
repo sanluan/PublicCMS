a33da0950bf3111f0105d882bf1330cdf1d99192
sanluan <master@publiccms.com>
2022-06-17 21:13:51 +0800

    增加脚本执行站点id判断

--- publiccms-parent/publiccms-core/src/main/java/com/publiccms/views/directive/tools/ExecuteScriptDirective.java
+++ publiccms-parent/publiccms-core/src/main/java/com/publiccms/views/directive/tools/ExecuteScriptDirective.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.publiccms.common.base.AbstractTemplateDirective;
+import com.publiccms.common.constants.CommonConstants;
 import com.publiccms.common.handler.RenderHandler;
+import com.publiccms.common.tools.LanguagesUtils;
 import com.publiccms.logic.component.site.ScriptComponent;
 
 /**
@@ -21,10 +23,15 @@ public class ExecuteScriptDirective extends AbstractTemplateDirective {
     public void execute(RenderHandler handler) throws IOException, Exception {
         String command = handler.getString("command");
         String[] parameters = handler.getStringArray("parameters");
-        try {
-            handler.print(scriptComponent.execute(command, parameters, handler.getLong("timeout", 1l)));
-        } catch (IOException | InterruptedException e) {
-            handler.print(e.getMessage());
+        if (siteComponent.isMaster(getSite(handler).getId())) {
+            try {
+                handler.print(scriptComponent.execute(command, parameters, handler.getLong("timeout", 1l)));
+            } catch (IOException | InterruptedException e) {
+                handler.print(e.getMessage());
+            }
+        } else {
+            handler.print(
+                    LanguagesUtils.getMessage(CommonConstants.applicationContext, handler.getLocale(), "verify.custom.noright"));
         }
         handler.render();
     }
