618e4ec8e3c30ee050f59188d6a309dd27441f97
sanluan <master@publiccms.com>
2023-01-05 21:43:14 +0800

    ui update

--- publiccms-parent/publiccms/src/main/webapp/resource/js/jui/ui.js
+++ publiccms-parent/publiccms/src/main/webapp/resource/js/jui/ui.js
@@ -44,6 +44,7 @@ function initEnv() {
                 var tabid = hash.substring(hash.indexOf('_') + 1);
                 if(tabid ) {
                     callback = function(){
+                        $('#menu a[rel='+escapeJquery(tabid)+']').closest('.accordionContent').prev().click();
                         $('#menu a[rel='+escapeJquery(tabid)+']').click();
                     }
                 }
