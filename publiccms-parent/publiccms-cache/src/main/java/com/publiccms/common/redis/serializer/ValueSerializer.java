c7bf58bf07fdc60a71134c6a73a4947c7709abf7
sanluan <master@publiccms.com>
2023-12-15 17:10:31 +0800

    https://github.com/sanluan/PublicCMS/issues/76

--- publiccms-parent/publiccms-cache/src/main/java/com/publiccms/common/redis/serializer/ValueSerializer.java
+++ publiccms-parent/publiccms-cache/src/main/java/com/publiccms/common/redis/serializer/ValueSerializer.java
@@ -1,53 +1,58 @@
 package com.publiccms.common.redis.serializer;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.lang.reflect.ParameterizedType;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.core.ConfigurableObjectInputStream;
 
+import com.publiccms.common.constants.Constants;
 import com.publiccms.common.tools.CommonUtils;
 
 /**
  *
- * BinarySerializer
+ * ValueSerializer
  * 
  * @param <T>
  * 
  */
-public class BinarySerializer<T> implements Serializer<T> {
+public class ValueSerializer<T> implements Serializer<T> {
     protected final Log log = LogFactory.getLog(getClass());
+    private Class<T> clazz;
 
     @Override
     public byte[] serialize(final T graph) {
         if (null == graph) {
             return EMPTY_BYTES;
         }
-        try (ByteArrayOutputStream os = new ByteArrayOutputStream(); ObjectOutputStream oos = new ObjectOutputStream(os)) {
-            oos.writeObject(graph);
-            oos.flush();
-            return os.toByteArray();
+        try {
+            return Constants.objectMapper.writeValueAsBytes(graph);
         } catch (Exception e) {
-            log.warn(CommonUtils.joinString("Fail to serializer graph. graph = ",graph), e);
+            log.warn(CommonUtils.joinString("Fail to serializer graph. graph = ", graph), e);
             return EMPTY_BYTES;
         }
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public T deserialize(final byte[] bytes) {
         if (null == bytes || 0 == bytes.length) {
             return null;
         }
         try (ByteArrayInputStream is = new ByteArrayInputStream(bytes);
                 ObjectInputStream ois = new ConfigurableObjectInputStream(is, Thread.currentThread().getContextClassLoader())) {
-            return (T) ois.readObject();
+            return Constants.objectMapper.readValue(bytes, getValueClass());
         } catch (Exception e) {
             log.warn("Fail to deserialize bytes.", e);
             return null;
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    public Class<T> getValueClass() {
+        return null == clazz
+                ? this.clazz = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]
+                : clazz;
     }
 }
