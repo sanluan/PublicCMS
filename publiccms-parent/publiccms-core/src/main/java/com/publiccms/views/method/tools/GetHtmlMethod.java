652d8e3d8197b96f7fdcd8a335304f44e1ccc5bc
sanluan <master@publiccms.com>
2024-08-25 07:39:55 +0800

    Merge remote-tracking branch 'origin/develop' into V5-develop

--- publiccms-parent/publiccms-core/src/main/java/com/publiccms/views/method/tools/GetHtmlMethod.java
+++ publiccms-parent/publiccms-core/src/main/java/com/publiccms/views/method/tools/GetHtmlMethod.java
@@ -7,9 +7,6 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
@@ -38,6 +35,8 @@ import com.publiccms.logic.component.config.SafeConfigComponent;
 import freemarker.core.Environment;
 import freemarker.template.TemplateModel;
 import freemarker.template.TemplateModelException;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  *
