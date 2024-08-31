f8fbbda8b61b6bea30e31718c415464f23400620
sanluan <master@publiccms.com>
2024-08-31 10:47:38 +0800

    bug fixed:tinymce click event

--- publiccms-parent/publiccms/src/main/webapp/resource/js/tinymce.config.js
+++ publiccms-parent/publiccms/src/main/webapp/resource/js/tinymce.config.js
@@ -36,7 +36,7 @@
                 var input = document.createElement('input');
                     input.setAttribute('type', 'file');
                     input.setAttribute('accept', filetype);
-                input.trigger("click");
+                input.click();
                 input.onchange = function() {
                     var file = this.files[0];
                     var xhr, formData;
