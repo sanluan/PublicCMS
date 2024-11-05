679f35e3a4c742765234a88b930d415980d0b001
sanluan <master@publiccms.com>
2024-11-05 12:38:54 +0800

    add fingerprint login

--- publiccms-parent/publiccms-oauth/src/main/java/com/publiccms/controller/admin/oauth/WebAuthnController.java
+++ publiccms-parent/publiccms-oauth/src/main/java/com/publiccms/controller/admin/oauth/WebAuthnController.java
@@ -12,26 +12,35 @@ import java.util.UUID;
 
 import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.publiccms.common.constants.CommonConstants;
 import com.publiccms.common.constants.Constants;
 import com.publiccms.common.tools.CommonUtils;
+import com.publiccms.common.tools.ControllerUtils;
 import com.publiccms.common.tools.ExtendUtils;
 import com.publiccms.common.tools.JsonUtils;
+import com.publiccms.common.tools.LanguagesUtils;
 import com.publiccms.common.tools.RequestUtils;
 import com.publiccms.controller.admin.LoginAdminController;
+import com.publiccms.entities.log.LogLogin;
 import com.publiccms.entities.sys.SysSite;
 import com.publiccms.entities.sys.SysUser;
 import com.publiccms.entities.sys.SysUserAttribute;
 import com.publiccms.entities.sys.SysUserToken;
 import com.publiccms.logic.component.config.ConfigDataComponent;
 import com.publiccms.logic.component.config.SafeConfigComponent;
+import com.publiccms.logic.component.config.SiteConfigComponent;
+import com.publiccms.logic.component.site.LockComponent;
 import com.publiccms.logic.service.log.LogLoginService;
 import com.publiccms.logic.service.sys.SysUserAttributeService;
 import com.publiccms.logic.service.sys.SysUserService;
@@ -54,6 +63,7 @@ import com.webauthn4j.data.PublicKeyCredentialUserEntity;
 import com.webauthn4j.data.RegistrationData;
 import com.webauthn4j.data.RegistrationParameters;
 import com.webauthn4j.data.UserVerificationRequirement;
+import com.webauthn4j.data.attestation.authenticator.AttestedCredentialData;
 import com.webauthn4j.data.attestation.statement.COSEAlgorithmIdentifier;
 import com.webauthn4j.data.client.Origin;
 import com.webauthn4j.data.client.challenge.Challenge;
@@ -80,10 +90,16 @@ public class WebAuthnController {
     private ConfigDataComponent configDataComponent;
     @Resource
     private SysUserTokenService sysUserTokenService;
+    @Resource
+    private LogLoginService logLoginService;
+    @Resource
+    private LockComponent lockComponent;
+    @Resource
+    protected LocaleResolver localeResolver;
 
     private WebAuthnManager webAuthnManager = WebAuthnManager.createNonStrictWebAuthnManager();
     private static List<PublicKeyCredentialParameters> pubKeyCredParams = new ArrayList<>();
-    private static long timeout = Duration.ofMinutes(5).toMillis();
+    private static long timeout = Duration.ofMinutes(1).toMillis();
     static {
         pubKeyCredParams
                 .add(new PublicKeyCredentialParameters(PublicKeyCredentialType.PUBLIC_KEY, COSEAlgorithmIdentifier.ES256));
@@ -99,7 +115,7 @@ public class WebAuthnController {
                 .add(new PublicKeyCredentialParameters(PublicKeyCredentialType.PUBLIC_KEY, COSEAlgorithmIdentifier.RS512));
     }
 
-    @GetMapping("attestation/options")
+    @PostMapping("attestation/options")
     public PublicKeyCredentialCreationOptions registerOptions(@SessionAttribute SysUser admin, HttpServletRequest request) {
         PublicKeyCredentialRpEntity rpEntity = new PublicKeyCredentialRpEntity(request.getServerName(), request.getServerName());
         PublicKeyCredentialUserEntity userEntity = new PublicKeyCredentialUserEntity(admin.getName().getBytes(), admin.getName(),
@@ -138,7 +154,7 @@ public class WebAuthnController {
     }
 
     @PostMapping("attestation/result")
-    public Map<String, Object> registerCredential(@RequestAttribute SysSite site, @SessionAttribute SysUser admin,
+    public Map<String, String> registerCredential(@RequestAttribute SysSite site, @SessionAttribute SysUser admin,
             @RequestBody String registrationResponseJSON, HttpServletRequest request) {
         Challenge challenge = new DefaultChallenge(admin.getName().getBytes());
         Origin origin = new Origin(request.getServerName());
@@ -168,13 +184,17 @@ public class WebAuthnController {
             } else {
                 webauthnMap = new HashMap<>();
             }
-            webauthnMap.put(Base64.getUrlEncoder().encodeToString(credentialRecord.getAttestedCredentialData().getCredentialId()),
-                    credentialRecord);
+            @SuppressWarnings("deprecation")
+            AttestedCredentialData attestedCredentialData = credentialRecord.getAttestedCredentialData();
+            webauthnMap.put(Base64.getUrlEncoder().encodeToString(attestedCredentialData.getCredentialId()), credentialRecord);
             map.put(SysUserAttributeService.SETTINGS_CODE_WEBAUTHN, JsonUtils.getString(webauthnMap));
             attributeService.updateSettings(admin.getId(), ExtendUtils.getExtendString(map, null));
-            return Collections.singletonMap("result", true);
+            return Collections.singletonMap("status", "ok");
         } catch (ValidationException e) {
-            return Collections.singletonMap("result", false);
+            Map<String, String> result = new HashMap<>();
+            result.put("status", "failed");
+            result.put("errorMessage", e.getMessage());
+            return result;
         }
     }
 
@@ -188,7 +208,7 @@ public class WebAuthnController {
     }
 
     @PostMapping("assertion/result")
-    public Map<String, Object> loginResult(@RequestAttribute SysSite site, @RequestBody String authenticationResponseJSON,
+    public Map<String, String> loginResult(@RequestAttribute SysSite site, @RequestBody String authenticationResponseJSON,
             @SessionAttribute String webauthnuser, HttpServletRequest request, HttpServletResponse response,
             HttpSession session) {
         Origin origin = new Origin(request.getServerName());
@@ -199,9 +219,35 @@ public class WebAuthnController {
         List<byte[]> allowCredentials = null;
         boolean userVerificationRequired = true;
         boolean userPresenceRequired = true;
-        SysUser admin = service.findByName(site.getId(), webauthnuser);
-        if (null != admin) {
-            SysUserAttribute attribute = attributeService.getEntity(admin.getId());
+        SysUser user = service.findByName(site.getId(), webauthnuser);
+        Map<String, String> result = new HashMap<>();
+        result.put("status", "failed");
+        result.put("errorMessage", LanguagesUtils.getMessage(CommonConstants.applicationContext,
+                localeResolver.resolveLocale(request), "verify.custom.password"));
+        String ip = RequestUtils.getIpAddress(request);
+        ModelMap model = new ModelMap();
+        boolean locked = lockComponent.isLocked(site.getId(), LockComponent.ITEM_TYPE_IP_LOGIN, ip, null);
+        if (ControllerUtils.errorCustom("locked.ip", locked && ControllerUtils.ipNotEquals(ip, user), model)
+                || ControllerUtils.errorNotEquals("password", user, model)) {
+            lockComponent.lock(site.getId(), LockComponent.ITEM_TYPE_IP_LOGIN, ip, null, true);
+            logLoginService.save(new LogLogin(site.getId(), webauthnuser, null == user ? null : user.getId(), ip,
+                    LogLoginService.CHANNEL_WEB_MANAGER, false, CommonUtils.getDate(), "fingerprint"));
+            result.put("errorMessage", (String) model.get(CommonConstants.ERROR));
+            return result;
+        }
+        if (null != user) {
+            locked = lockComponent.isLocked(site.getId(), LockComponent.ITEM_TYPE_LOGIN, String.valueOf(user.getId()), null);
+            if (ControllerUtils.errorCustom("locked.user", locked, model) || LoginAdminController.verifyNotAdmin(user, model)
+                    || LoginAdminController.verifyNotEnablie(user, model)) {
+                Long userId = user.getId();
+                lockComponent.lock(site.getId(), LockComponent.ITEM_TYPE_LOGIN, String.valueOf(user.getId()), null, true);
+                lockComponent.lock(site.getId(), LockComponent.ITEM_TYPE_IP_LOGIN, ip, null, true);
+                logLoginService.save(new LogLogin(site.getId(), webauthnuser, userId, ip, LogLoginService.CHANNEL_WEB_MANAGER,
+                        false, CommonUtils.getDate(), "fingerprint"));
+                result.put("errorMessage", (String) model.get(CommonConstants.ERROR));
+                return result;
+            }
+            SysUserAttribute attribute = attributeService.getEntity(user.getId());
             Map<String, String> map = ExtendUtils.getSettingsMap(attribute);
             String webauthn = map.get(SysUserAttributeService.SETTINGS_CODE_WEBAUTHN);
             if (CommonUtils.notEmpty(webauthn)) {
@@ -215,8 +261,7 @@ public class WebAuthnController {
                         AuthenticationParameters authenticationParameters = new AuthenticationParameters(serverProperty,
                                 credentialRecord, allowCredentials, userVerificationRequired, userPresenceRequired);
                         webAuthnManager.verify(authenticationData, authenticationParameters);
-                        String ip = RequestUtils.getIpAddress(request);
-                        service.updateLoginStatus(admin.getId(), ip);
+                        service.updateLoginStatus(user.getId(), ip);
                         String authToken = UUID.randomUUID().toString();
                         Date now = CommonUtils.getDate();
                         Map<String, String> safeConfig = configDataComponent.getConfigData(site.getId(),
@@ -224,16 +269,29 @@ public class WebAuthnController {
                         int expiryMinutes = ConfigDataComponent.getInt(
                                 safeConfig.get(SafeConfigComponent.CONFIG_EXPIRY_MINUTES_MANAGER),
                                 SafeConfigComponent.DEFAULT_EXPIRY_MINUTES);
-                        LoginAdminController.addLoginStatus(admin, authToken, request, response, expiryMinutes);
-                        sysUserTokenService.save(new SysUserToken(authToken, site.getId(), admin.getId(),
+                        LoginAdminController.addLoginStatus(user, authToken, request, response, expiryMinutes);
+                        sysUserTokenService.save(new SysUserToken(authToken, site.getId(), user.getId(),
                                 LogLoginService.CHANNEL_WEB_MANAGER, now, DateUtils.addMinutes(now, expiryMinutes), ip));
+                        logLoginService.save(new LogLogin(site.getId(), webauthnuser, user.getId(), ip,
+                                LogLoginService.CHANNEL_WEB_MANAGER, false, CommonUtils.getDate(), "fingerprint"));
                         session.removeAttribute("webauthnuser");
-                        return Collections.singletonMap("result", true);
+                        return Collections.singletonMap("status", "ok");
                     }
                 } catch (JsonProcessingException | VerificationException e) {
+                    result.put("errorMessage", e.getMessage());
                 }
             }
         }
-        return Collections.singletonMap("result", false);
+        return result;
+    }
+
+    @GetMapping("assertion/login")
+    public String login(@RequestAttribute SysSite site, @RequestBody String returnUrl, HttpServletRequest request) {
+        Map<String, String> config = configDataComponent.getConfigData(site.getId(), SiteConfigComponent.CONFIG_CODE);
+        String safeReturnUrl = config.get(SafeConfigComponent.CONFIG_RETURN_URL);
+        if (SafeConfigComponent.isUnSafeUrl(returnUrl, site, safeReturnUrl, request.getContextPath())) {
+            returnUrl = CommonConstants.getDefaultPage();
+        }
+        return CommonUtils.joinString(UrlBasedViewResolver.REDIRECT_URL_PREFIX, returnUrl);
     }
 }
