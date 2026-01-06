d076096b46355038cf0bc0a91b65859b83071744
sanluan <master@publiccms.com>
2026-01-06 17:13:59 +0800

    bugfix

--- publiccms-parent/publiccms-core/src/main/java/com/publiccms/logic/component/template/MetadataComponent.java
+++ publiccms-parent/publiccms-core/src/main/java/com/publiccms/logic/component/template/MetadataComponent.java
@@ -265,7 +265,8 @@ public class MetadataComponent implements Cache {
      * @return template metadata map
      */
     private Map<String, CmsPageData> getTemplateDataMap(String dirPath, String lang) {
-        Map<String, CmsPageData> dataMap = pageDataCache.get(dirPath);
+        String cacheKey = CommonUtils.joinString(dirPath, Constants.SEPARATOR, lang);
+        Map<String, CmsPageData> dataMap = pageDataCache.get(cacheKey);
         if (null == dataMap) {
             String fileName = null;
             if (CommonUtils.notEmpty(lang)) {
@@ -284,7 +285,7 @@ public class MetadataComponent implements Cache {
             } else {
                 dataMap = new CaseInsensitiveMap<>();
             }
-            pageDataCache.put(dirPath, dataMap);
+            pageDataCache.put(cacheKey, dataMap);
         }
         return dataMap;
     }
