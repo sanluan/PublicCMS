81c1c4dc4b8971d7ecbe6258c3bda549930af442
sanluan <master@publiccms.com>
2026-01-19 11:09:02 +0800

    bugfix

--- publiccms-parent/publiccms-core/src/main/java/com/publiccms/interceptor/MultilingualInterceptor.java
+++ publiccms-parent/publiccms-core/src/main/java/com/publiccms/interceptor/MultilingualInterceptor.java
@@ -4,11 +4,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.publiccms.common.constants.CommonConstants;
 import com.publiccms.common.tools.RequestUtils;
 
+/**
+ * MultilingualInterceptor 多语言切换拦截器
+ * 
+ */
+@Component
 public class MultilingualInterceptor implements HandlerInterceptor {
     public static final String DEFAULT_PARAM_NAME = "language";
 
