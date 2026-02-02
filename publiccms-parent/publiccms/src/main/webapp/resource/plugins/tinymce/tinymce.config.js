22ed15fdedf19888c0b09a8b24538f2bd7efd690
sanluan <master@publiccms.com>
2026-02-02 12:27:59 +0800

    disable sticky toolbar

--- publiccms-parent/publiccms/src/main/webapp/resource/plugins/tinymce/tinymce.config.js
+++ publiccms-parent/publiccms/src/main/webapp/resource/plugins/tinymce/tinymce.config.js
@@ -2,12 +2,11 @@
     window.TINYMCE_OPTIONS = {
             language: window.TIMYMCE_LANGUAGE,
             base_url: window.TIMYMCE_HOME_URL,
-            plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+            plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
             editimage_cors_hosts: ['picsum.photos'],
             menubar: 'file edit view insert format tools table help',
-            toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media link anchor codesample template | ltr rtl',
-            toolbar_sticky: true,
-            toolbar_sticky_offset: 108,
+            toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media link anchor codesample | ltr rtl',
+            toolbar_sticky: false,
             autosave_ask_before_unload: true,
             autosave_interval: '30s',
             autosave_prefix: '{path}{query}-{id}-',
@@ -116,13 +115,6 @@
                     xhr.send(formData);
               };
           },
-          templates: [
-            { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
-            { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
-            { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
-          ],
-          template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
-          template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
           height: 600,
           image_caption: true,
           quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
