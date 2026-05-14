9b650d7e6ea9324965385ae9dbde849ba98468b2
sanluan <master@publiccms.com>
2026-05-14 19:32:42 +0800

    bugfix

--- publiccms-parent/publiccms-trade/src/main/java/com/publiccms/logic/service/trade/TradeAddressService.java
+++ publiccms-parent/publiccms-trade/src/main/java/com/publiccms/logic/service/trade/TradeAddressService.java
@@ -1,17 +1,19 @@
 package com.publiccms.logic.service.trade;
 
+import java.util.List;
+
 // Generated 2023-8-16 by com.publiccms.common.generator.SourceGenerator
 
 import javax.annotation.Resource;
 
-
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.publiccms.entities.trade.TradeAddress;
-import com.publiccms.logic.dao.trade.TradeAddressDao;
 import com.publiccms.common.base.BaseService;
 import com.publiccms.common.handler.PageHandler;
+import com.publiccms.entities.trade.TradeAddress;
+import com.publiccms.logic.dao.trade.TradeAddressDao;
 
 /**
  *
@@ -30,10 +32,18 @@ public class TradeAddressService extends BaseService<TradeAddress> {
      * @return results page
      */
     @Transactional(readOnly = true)
-    public PageHandler getPage(Short siteId, Long userId, 
-                Integer pageIndex, Integer pageSize) {
-        return dao.getPage(siteId, userId, 
-                pageIndex, pageSize);
+    public PageHandler getPage(Short siteId, Long userId, Integer pageIndex, Integer pageSize) {
+        return dao.getPage(siteId, userId, pageIndex, pageSize);
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public void delete(short siteId, Long[] ids, Long userId) {
+        List<TradeAddress> entityList = getEntitys(ids);
+        for (TradeAddress entity : entityList) {
+            if (null != entity && siteId == entity.getSiteId() && (null == userId || entity.getUserId() == userId)) {
+                dao.delete(entity);
+            }
+        }
     }
     
     @Resource
