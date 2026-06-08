83d6bdd252375d9816ae80ae24094a7f4fabdba5
sanluan <master@publiccms.com>
2026-06-08 16:15:34 +0800

    bugfix

--- publiccms-parent/publiccms-core/src/main/resources/initialization/sql/202406-develop.sql
+++ publiccms-parent/publiccms-core/src/main/resources/initialization/sql/202406-develop.sql
@@ -129,6 +129,6 @@ ALTER TABLE `cms_word`
   DROP INDEX `cms_word_hidden`,
   ADD INDEX  `cms_word_hidden` (`site_id`,`lang`, `hidden`);
 -- 2026-05-26 --
-DELETE FROM sys_module WHERE module_id in ('myself_content_view','myself_process_view');
+DELETE FROM sys_module WHERE id in ('myself_content_view','myself_process_view');
 -- 2026-06-08 --
 ALTER TABLE `cms_category` ADD COLUMN `parent_ids` text NULL COMMENT '所有父分类' AFTER `type_id`;
